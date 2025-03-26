672203780eaf0bd69dd98f92089be2689da239de
kobzar2 <prokharchyk2003@gmail.com>
2025-03-26 15:46:01 +0100

    Lab4_1

--- src/mojaCzwartaAplikacja.java
+++ src/mojaCzwartaAplikacja.java
@@ -4,7 +4,7 @@ public class mojaCzwartaAplikacja {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         while (true) {
-            int k=1;
+            int k=2;
             System.out.print("Podaj min i max limity:");
             int min = scanner.nextInt();
             int max = scanner.nextInt();
