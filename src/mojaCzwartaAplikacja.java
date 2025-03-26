187ff66eee6debc7208641c8df981877947e1ed3
kobzar2 <prokharchyk2003@gmail.com>
2025-03-26 15:18:56 +0100

    Lab4_1

---
+++ src/mojaCzwartaAplikacja.java
@@ -0,0 +1,28 @@
+
+import java.util.Scanner;
+public class mojaCzwartaAplikacja {
+    public static void main(String[] args) {
+        mojaCzwartaAplikacja app = new mojaCzwartaAplikacja();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.print("Podaj min i max limity:");
+            int min = scanner.nextInt();
+            int max = scanner.nextInt();
+            if (min >= max) {
+                System.out.print("Max powinien byc wiekszy, niz Min. Sproboj ponownie.");
+                break;
+            }
+            int square1 = min * min;
+            int square2 = max * max;
+
+            int sumaKwadratow = 0;
+            for (int i = min; i <= max; i++) {
+                int kwadrat = i * i;
+                sumaKwadratow += kwadrat;
+            }
+            System.out.println(square1);
+            System.out.println(square2);
+            System.out.println(+sumaKwadratow);
+        }
+    }
+}
